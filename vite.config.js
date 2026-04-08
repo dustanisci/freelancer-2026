f3453f83f32558bb06f2419ccd9e35bed388b6c7
Edu Stanisci <dustanisci@outlook.com>
2026-04-07 23:43:10 -0300

    build: improve application build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   root: '.',
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 75 },
+      jpg: { quality: 75 },
+      jpeg: { quality: 75 },
+      webp: { lossless: false, quality: 75 },
+      svg: {
+        plugins: [
+          { name: 'removeViewBox', active: false },
+          { name: 'cleanupIDs', active: false },
+        ],
+      },
+    }),
+  ],
   build: {
     outDir: 'dist',
     target: 'esnext',
     minify: 'esbuild',
+    cssMinify: true,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['animejs', 'bootstrap'],
+        },
+      },
+    },
   },
   server: {
     open: true,
